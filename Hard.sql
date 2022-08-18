3d3f9db43c8ce11616317383e18ae3cb369c37e9
Nadine Pereira <81249310+nadine-e-pereira@users.noreply.github.com>
2022-08-18 14:58:47 -0300

    Update Hard.sql

--- Hard.sql
+++ Hard.sql
@@ -72,5 +72,13 @@ GROUP BY PR.province_name
 HAVING COUNT( CASE WHEN gender = 'M' THEN 1 END) > COUNT( CASE WHEN gender = 'F' THEN 1 END);
 
 /*
-
+We are looking for a specific patient. Pull all columns for the patient who matches the following criteria:
+- First_name contains an 'r' after the first two letters.
+- Identifies their gender as 'F'
+- Born in February, May, or December
+- Their weight would be between 60kg and 80kg
+- Their patient_id is an odd number
+- They are from the city 'Halifax'
 */
+
+
