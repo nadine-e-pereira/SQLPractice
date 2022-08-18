a827e1f1e5bd4569dfebbf4877ad2b80c19d9171
Nadine Pereira <81249310+nadine-e-pereira@users.noreply.github.com>
2022-08-18 14:29:50 -0300

    Update Hard.sql

--- Hard.sql
+++ Hard.sql
@@ -9,3 +9,23 @@ SELECT COUNT(*) AS patients_in_group, FLOOR(weight / 10) * 10 AS weight_group
 FROM patients
 GROUP BY weight_group
 ORDER BY weight_group DESC;
+
+/*
+Show patient_id, weight, height, isObese from the patients table.
+Display isObese as a boolean 0 or 1.
+Obese is defined as weight(kg)/(height(m)2) >= 30.
+weight is in units kg.
+height is in units cm.
+*/
+
+SELECT patient_id, weight, height, 
+(CASE WHEN weight/(POWER(height/100.0,2)) >= 30 THEN 1 ELSE 0 END) AS isObese
+FROM patients;
+
+/*
+
+Show patient_id, first_name, last_name, and attending physician's specialty.
+Show only the patients who has a primary_diagnosis as 'Dementia' and the physician's first name is 'Lisa'
+Check patients, admissions, and physicians tables for required information.
+*/
+
