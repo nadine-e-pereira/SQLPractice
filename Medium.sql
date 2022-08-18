a674226344e4330c038d025a5d56c51978cbfa9b
Nadine Pereira <81249310+nadine-e-pereira@users.noreply.github.com>
2022-08-18 14:01:30 -0300

    Update Medium.sql

--- Medium.sql
+++ Medium.sql
@@ -106,3 +106,81 @@ WHERE NOT allergies = 'NKA'AND allergies NOT NULL
 GROUP BY allergies
 ORDER BY total_diagnosis DESC
 
+/*
+Show all patient's first_name, last_name, and birth_date who were born in the 1970s decade. 
+Sort the list starting from the earliest birth_date.
+*/
+
+SELECT first_name, last_name, birth_date
+FROM patients
+WHERE YEAR(birth_date) BETWEEN 1970 AND 1979
+ORDER BY birth_date ASC
+
+/*
+We want to display each patient's full name in a single column. Their last_name in all upper 
+letters must appear first, then first_name in all lower case letters. Separate the last_name and 
+first_name with a comma. Order the list by the first_name in decending order
+EX: SMITH,jane
+*/
+
+SELECT concat(UPPER(last_name),',',LOWER(first_name)) AS "full_name"
+FROM patients
+ORDER BY first_name DESC
+
+/*
+Show the province_id(s), sum of height; where the total sum of its patient's height is greater than 
+or equal to 7,000.
+*/
+
+SELECT province_id, SUM(height) AS sum_height
+FROM patients
+GROUP BY province_id
+HAVING sum_height >= 7000
+
+/*
+Show the difference between the largest weight and smallest weight for patients with the last name 'Maroni'
+*/
+
+SELECT (MAX(weight)-MIN(weight)) AS "weight_difference"
+FROM patients
+WHERE last_name = 'Maroni'
+
+/*Show all of the month's day numbers and how many admission_dates occurred on that number. 
+Sort by the day with most admissions to least admissions.
+*/
+
+SELECT DAY(admission_date) AS "day_number", COUNT(*) AS "number_of_admissions"
+FROM admissions
+GROUP BY day_number
+ORDER BY number_of_admissions DESC
+
+/*
+Show the patient_id, nursing_unit_id, room, and bed for patient_id 542's most recent admission_date.
+*/
+
+SELECT patient_id, nursing_unit_id, room, bed
+FROM admissions
+WHERE patient_id = 542 
+GROUP BY patient_id
+HAVING MAX(admission_date)
+
+/*
+Show the nursing_unit_id and count of admissions for each nursing_unit_id. Exclude the following 
+nursing_unit_ids: 'CCU', 'OR', 'ICU', 'ER'.
+*/
+
+SELECT nursing_unit_id, COUNT(patient_id) AS "total_admission"
+FROM admissions
+WHERE nursing_unit_id NOT IN ('CCU', 'OR', 'ICU', 'ER')
+GROUP BY nursing_unit_id
+
+/*
+Show patient_id, attending_physician_id, and primary_diagnosis for admissions that match one of the two criteria:
+1. patient_id is an odd number and attending_physician_id is either 1, 5, or 19.
+2. attending_physician_id contains a 2 and the length of patient_id is 3 characters.
+*/
+
+SELECT patient_id, attending_physician_id, primary_diagnosis
+FROM admissions
+WHERE (patient_id %2 = 1 AND attending_physician_id IN(1,5,19))
+OR (attending_physician_id LIKE '%2%' AND LEN(patient_id) = 3)
