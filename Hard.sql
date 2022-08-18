70f75f11bfa7ffe59abadcf76a5495a29308e734
Nadine Pereira <81249310+nadine-e-pereira@users.noreply.github.com>
2022-08-18 14:03:54 -0300

    Update Hard.sql

--- Hard.sql
+++ Hard.sql
@@ -1,1 +1,11 @@
+/*
+Show all of the patients grouped into weight groups.
+Show the total amount of patients in each weight group.
+Order the list by the weight group decending.
+For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.
+*/
 
+SELECT COUNT(*) AS patients_in_group, FLOOR(weight / 10) * 10 AS weight_group
+FROM patients
+GROUP BY weight_group
+ORDER BY weight_group DESC;
